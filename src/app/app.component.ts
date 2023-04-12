9085c9373c78468b6ae8432c8e58cbbca03fe6e6
brandontyruspetty <tyrus1974@gmail.com>
2023-04-12 14:34:28 -0700

    update app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myNoirMovies-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  //this is the function that will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      //assigning the dialog a width
+      width: '280px'
+    });
+  }
+  //this is the function that will open the dialog when the login button is clicked
+  openLoginFormDialog(): void {
+    this.dialog.open(LoginFormComponent, {
+      //assigning the dialog a width
+      width: '280px'
+    });
+  }
+
 }
