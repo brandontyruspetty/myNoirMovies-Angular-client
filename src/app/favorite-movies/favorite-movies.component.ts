8f878fb9079dcbfeef44fbcfcfb5d060604d01d1
brandontyruspetty <tyrus1974@gmail.com>
2023-04-24 12:14:05 -0700

    Update favorite-movies.component.ts

--- src/app/favorite-movies/favorite-movies.component.ts
+++ src/app/favorite-movies/favorite-movies.component.ts
@@ -6,17 +6,19 @@ import { SynopsisPageComponent } from '../synopsis-page/synopsis-page.component'
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-favorite-movies',
   templateUrl: './favorite-movies.component.html',
   styleUrls: ['./favorite-movies.component.scss']
 })
+
+/**
+ * the FavoriteMoviesComponent class fetches and displays the user's
+ * favorite movies in card format in the user profile page
+ */
 export class FavoriteMoviesComponent implements OnInit {
-  favorites: any[] = [];
-  favoriteMovies: any[] = [];
-
-
+  movies: any[] = [];             // array of movie ids
+  favoriteMovies: any[] = [];     // array of movie objects
 
   constructor(
     public fetchApiData: FetchApiDataService,
@@ -28,25 +30,44 @@ export class FavoriteMoviesComponent implements OnInit {
     this.getFavoriteMovies();
   }
 
+  /**
+   * called OnInit and when the favorite (heart) icon is clicked
+   * 
+   * empties this.favorites[] and this.favoriteMovies[], an array of movie ids and movie objects
+   * 
+   * then fetches the user's favorite movies and sets this.favorites as the response.
+   * 
+   * then, using the map method, for each favorite (string) in this.favorites[],
+   * fetch the movie (object) by the id and push the response into this.favoriteMovies[].
+   */
 
   getFavoriteMovies(): void {
-    this.favorites, this.favoriteMovies = [];
+    this.movies, this.favoriteMovies = [];
     this.fetchApiData.getUser().subscribe((resp: any) => {
-      this.favorites = resp.FavoriteMovies;
-      this.favorites.map((favorite: any) => {
+      this.movies = resp.FavoriteMovies;
+      this.movies.map((favorite: any) => {
         console.log(favorite);
         this.fetchApiData.getMovieById(favorite).subscribe((resp: any) => {
           this.favoriteMovies.push(resp);
         });
       });
+      console.log("*** DEBUG: favoriteMovies:", this.favoriteMovies)
     });
   }
-  //check if a movie is a user's favorite
+
+  /**
+   * Check if a movie id is included in the user's favorites
+   * @param id
+   * @returns a boolean value
+   */
   isFavorite(id: string): boolean {
     return this.favoriteMovies.includes(id);
   }
 
-  //add a movie to a user's favorites
+  /**
+   * Add a movie id into the user's favorites with FetchApiDataService.addFavoriteMovie()
+   * @param id
+   */
   addToFavorites(id: string): void {
     this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
       this.snackBar.open('Movie added to favorites', 'OK', {
@@ -56,7 +77,12 @@ export class FavoriteMoviesComponent implements OnInit {
     });
   }
 
-  //remove a movie from a user's favorites
+
+  /**
+ * Delete one movie id from the user's favorites with FetchApiDataService.removeFavoriteMovie()
+ * @param id
+ */
+
   deleteFromFavorites(id: string): void {
     console.log(id);
     this.fetchApiData.deleteFavoriteMovie(id).subscribe((results) => {
