c243632f357515f37921563400075a71be4510c2
brandontyruspetty <tyrus1974@gmail.com>
2023-04-26 16:10:27 -0700

    Update welcome-page.component.ts

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -10,30 +10,35 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
+
+/**
+ * the WelcomePageComponent is the default view if the user is not logged in, 
+ * displaying a welcome message, a login button and a new user registration button
+ */
 export class WelcomePageComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
   ngOnInit(): void { }
 
 
-  //this is the function that will open the dialog when the signup button is clicked
+  /** 
+   * this is the function that will open the dialog 
+   * when the signup button is clicked
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       //assigning the dialog a width
       width: '280px'
     });
   }
-  //this is the function that will open the dialog when the login button is clicked
+  /** 
+   * this is the function that will open the 
+   * dialog when the login button is clicked 
+  */
   openLoginFormDialog(): void {
     this.dialog.open(LoginFormComponent, {
       //assigning the dialog a width
       width: '280px'
     });
   }
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
-
 
 }
